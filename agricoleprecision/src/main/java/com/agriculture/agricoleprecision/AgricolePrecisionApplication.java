63b8196c1b6060aa32df1e21a4da1c08f9fa5fd2
habibdrira <habibdrira6@gmail.com>
2025-05-31 10:36:35 +0100

    first

---
+++ agricoleprecision/src/main/java/com/agriculture/agricoleprecision/AgricolePrecisionApplication.java
@@ -0,0 +1,13 @@
+package com.agriculture.agricoleprecision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgricolePrecisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgricolePrecisionApplication.class, args);
+	}
+
+}
---
+++ agricoleprecision/src/test/java/com/agriculture/agricoleprecision/AgricolePrecisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agriculture.agricoleprecision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgricolePrecisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
